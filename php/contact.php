242f2f8fca55adfdb7ee0373cf358a6325dcd27d
Christophe D'Anna <119214025+MrBabon@users.noreply.github.com>
2023-05-31 22:44:50 +0200

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,89 +1,88 @@
 <?php
-    $array = array("firstname" => "", "name" => "", "email" => "", "phone" => "", "message" => "", "firstnameError" => "", "nameError" => "", "emailError" => "", "phoneError" => "", "messageError" => "", "isSuccess" => false);
+$array = array(
+    "firstname" => "",
+    "name" => "",
+    "email" => "",
+    "phone" => "",
+    "message" => "",
+    "firstnameError" => "",
+    "nameError" => "",
+    "emailError" => "",
+    "phoneError" => "",
+    "messageError" => "",
+    "isSuccess" => false
+);
 
-    $emailTo = "christophedanna06@gmail.com";
+$emailTo = "christophedanna06@gmail.com";
 
-    if($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        $array["firstname"] = verifyInput($_POST["firstname"]);
-        $array["name"] = verifyInput($_POST["name"]);
-        $array["email"] = verifyInput($_POST["email"]);
-        $array["phone"] = verifyInput($_POST["phone"]);
-        $array["message"] = verifyInput($_POST["message"]);
-        $array["isSuccess"] = true;
-        $emailText = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $array["firstname"] = verifyInput($_POST["firstname"]);
+    $array["name"] = verifyInput($_POST["name"]);
+    $array["email"] = verifyInput($_POST["email"]);
+    $array["phone"] = verifyInput($_POST["phone"]);
+    $array["message"] = verifyInput($_POST["message"]);
+    $array["isSuccess"] = true;
+    $emailText = "";
 
-        if(empty($array["firstname"]))
-        {
-            $array["firstnameError"] = "Je veux connaitre ton prénom !";
-            $array["isSuccess"] = false;
-
-        }
-        else
-            $emailText .= "Firstname:{$array["firstname"]}\n";
-
-        if(empty($array["name"]))
-        {
-            $array["nameError"] = "Et oui je veux tout savoir. Même ton nom !";
-            $array["isSuccess"] = false;
-            
-        }
-        else
-            $emailText .= "Name: {$array["name"]}\n";
-
-        if(!isEmail($array["email"]))
-        {
-            $array["emailError"] = "T'essaies de me rouler ? C'est pas un email ça ?";
-            $array["isSuccess"] = false;
-                
-        }
-        else
-        $emailText .= "Email: {$array["email"]}\n";
-        
-        if(!isPhone($array["phone"]))
-        {
-            $array["phoneError"] = "Que des chiffres et des espaces, conasse";
-            $array["isSuccess"] = false;
-        }
-        else
-        $emailText .= "Phone: {$array["phone"]}\n";
-
-        if(empty($array["message"]))
-        {
-            $array["messageError"] = "Tu souhaite me dire quoi ?";
-            $array["isSuccess"] = false;
-            
-        }
-        else
-            $emailText .= "Message: {$array["message"]}\n";
-
-        
-
-        
-        
-        if($array["isSuccess"])
-        {
-            $headers = "From: {$array["firstname"]} {$array["name"]} <{$array["email"]}>\r\nReply-To: {$array["email"]}";
-            mail($emailTo,"Un message de votre site", $emailText , $headers);
-            
-
-        }
-
-        echo json_encode($array);
-        
-
-    }
-    
-    function isPhone($var)
-    {
-        return preg_match("/^[0-9 ]*$/", $var);
+    if (empty($array["firstname"])) {
+        $array["firstnameError"] = "Veuillez indiquer votre prénom.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Prénom : {$array["firstname"]}\n";
     }
 
-    function isEmail($var)
-    {
-        return filter_var($var, FILTER_VALIDATE_EMAIL);
-
+    if (empty($array["name"])) {
+        $array["nameError"] = "Veuillez indiquer votre nom.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Nom : {$array["name"]}\n";
     }
+
+    if (!isEmail($array["email"])) {
+        $array["emailError"] = "Veuillez fournir une adresse e-mail valide.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Email : {$array["email"]}\n";
+    }
+
+    if (!isPhone($array["phone"])) {
+        $array["phoneError"] = "Veuillez entrer un numéro de téléphone valide contenant uniquement des chiffres et des espaces.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Téléphone : {$array["phone"]}\n";
+    }
+
+    if (empty($array["message"])) {
+        $array["messageError"] = "Veuillez entrer votre message.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Message : {$array["message"]}\n";
+    }
+
+    if ($array["isSuccess"]) {
+        // Utilisation de la bibliothèque PHPMailer pour l'envoi d'e-mails
+        require 'path/to/PHPMailer/PHPMailer.php';
+        require 'path/to/PHPMailer/SMTP.php';
+        require 'path/to/PHPMailer/Exception.php';
+
+        $mail = new PHPMailer\PHPMailer\PHPMailer();
+
+        // Configuration du serveur SMTP (exemple pour Gmail)
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'your-email@gmail.com';
+        $mail->Password = 'your-email-password';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Configuration de l'expéditeur et du destinataire
+        $mail->setFrom($array["email"], "{$array["firstname"]} {$array["name"]}");
+        $mail->addAddress($emailTo);
+
+        // Sujet et corps de l'e-mail
+        $mail->Subject = 'Un message de votre
+
 
     // POUR SECURISER LE SITE
 
