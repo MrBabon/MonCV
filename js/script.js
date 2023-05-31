6131850c2414a43be5c5500f274a67fbe1f4d439
Christophe D'Anna <119214025+MrBabon@users.noreply.github.com>
2023-05-31 22:46:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,27 @@
 $(function(){
     $(".navbar a, footer a").on("click", function(event){
-
         event.preventDefault();
         var hash = this.hash;
-
-        $('body').animate({scrollTop: $(hash).offset().top}, 900, function(){window.location.hash = hash;})
+        $('body').animate({scrollTop: $(hash).offset().top}, 900, function(){
+            window.location.hash = hash;
+        });
     });
 
     $('#contact-form').submit(function(e){
-
         e.preventDefault();
         $('.comments').empty();
         var postdata = $('#contact-form').serialize();
 
         $.ajax({
-            type:'POST',
-            url:'php/contact.php',
+            type: 'POST',
+            url: 'php/contact.php',
             data: postdata,
-            dataType:'json',
+            dataType: 'json',
             success: function(result){
-                if(result.isSuccess)
-                {
-                    $("#contact-form").append("<p class='thank-you'>Votre message à bien été envoyé. Merci de m'avoir contacté </p>");
+                if(result.isSuccess) {
+                    $("#contact-form").append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté.</p>");
                     $("#contact-form")[0].reset();
-                }
-                else
-                {
+                } else {
                     $("#firstname + .comments").html(result.firstnameError);
                     $("#name + .comments").html(result.nameError);
                     $("#email + .comments").html(result.emailError);
@@ -35,4 +31,4 @@ $(function(){
             }
         });
     });
-})
+});
